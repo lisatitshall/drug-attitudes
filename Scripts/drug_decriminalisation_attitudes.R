36cfe3d70eacf6e9b0a5ac25c244fe324d8f5d11
lisatitshall <lisatitshall@gmail.com>
2025-02-13 16:16:56 +0000

    initial cleaning and exploration

--- Scripts/drug_decriminalisation_attitudes.R
+++ Scripts/drug_decriminalisation_attitudes.R
@@ -3,28 +3,140 @@ library(tidyverse)
 library(tidymodels)
 library(readxl)
 
+#Data Notes
+# AGE:PARTY columns are demographics questions
+# HARDSOFT:MORALITY are 9 Likert questions (where 5 means favour decriminalisation)
+# RESOURCES:OTHERNOCONCERN select all question about no concern
+# CRIME RATE:OTHERCONCERN select all question about concern
+# LAW is decriminalisation question explicitly asked (Likert)
+# WORRIED? is concern about drug decriminalisation 
 
-#datasets need to be combined because they show different things
-#remove row with memorable ID 2804JG, data was incomplete
-#lots of ethnicities - probably not enough to analyse / compare - remove
+#Aim
+# Predict LAW/WORRIED category based on other answers
+# Analyse demographics impact on LAW/WORRIED answers (if any)
+
+#import data from xlsx
+demographics <- 
+  read_excel("Data/Demographics_and_Attitude_Towards_Drug_Decriminalisation_Policy_Data_Set.xlsx")
+
+concerns <- 
+  read_excel("Data/Concerns_Toward_Drug_Decriminalisation_and_Demographics_Data_Set.xlsx")
+
+#join datasets, ID 1703SM isn't matching when it should
+#had to fix problem with Law numbers not matching up across two datasets
+all_data_raw <- inner_join(demographics, concerns, 
+                           by = c("MEMORABLE ID", "AGE", "GENDER", "EDUCATION", "LEANING", "LAW"), 
+                           keep = FALSE )
+
+#rename other columns and ones with unacceptable punctuation
+all_data_raw <- all_data_raw %>% 
+  rename("OTHERNOCONCERN" = `OTHER...14`,
+         "OTHERCONCERN" = `OTHER...20`, 
+         "USESELLING" = `USESELLING*`,
+         "PRISONTREATMENT" = `PRISONTREATMENT*`,
+         "MORALITY" = `MORALITY*`,
+         "CRIMERATE" = `CRIME RATE`, 
+         "NOPRISON" = `NO PRISON`,
+         "WORRIED" = `WORRIED?`)
+
+#rearrange columns
+all_data_raw <- all_data_raw %>% 
+  relocate(PARTY, .after = LEANING) %>%
+  relocate(LAW, .after = OTHERCONCERN) %>% 
+  relocate(WORRIED, .after = LAW)
+
+#remove row with null value
+all_data_amended <- na.omit(all_data_raw)
+
+#remove favourability score for now, not convinced they should include
+# the law question
+all_data_amended <- all_data_amended %>% select(!starts_with("Favourability"))
+
+#don't need ID column any more
+all_data_amended <- all_data_amended %>% select(AGE:WORRIED)
+
+#lots of ethnicities - probably not enough to analyse / compare
 #have demographics coded/not coded for visualisations
+glimpse(all_data_amended)
 
-#concerns sheet
-# demographics
-# decriminalisation question
-# concern about drug decriminalisation question
-# two select all questions about concerning / not concerning reasons
-#  1 means they chose that reason
+#everything should be a factor instead of a double
+all_data_amended <- as.data.frame(sapply(all_data_amended, as.factor), 
+                                  stringsAsFactors = TRUE)
 
-#demographics sheet
-# demographics
-# 10 likert questions were answered (1-5 scale where 5 means favour decriminalisation)
-# these answers were added up and 3 categories were chosen
-#  18-28 unfavourable
-#  29-35 neutral
-#  36-46 favourable
-#BUT law question was specifically about decriminalisation to see how that
-# answer compared to the other questions
+#ok
+glimpse(all_data_amended)
 
-#aim predict "worried" category based on other answers
-#what about law question though...
+#understand demographics
+
+#add age group 
+all_data_amended$AGERAW <- as.factor(
+  case_when(
+    all_data_amended$AGE == "1" ~ "18-25",
+    all_data_amended$AGE == "2" ~ "26-35",
+    all_data_amended$AGE == "3" ~ "36-45",
+    all_data_amended$AGE == "4" ~ "46-55",
+    all_data_amended$AGE == "5" ~ "56-65",
+    all_data_amended$AGE == "6" ~ "66+"
+))
+
+#apart from 66+ (arguably) enough of each
+plot(all_data_amended$AGERAW, xlab = "Age Groups", ylab = "Participants",
+     main = "Age Breakdown")
+
+#add gender
+all_data_amended$GENDERRAW <- as.factor(
+  case_when(
+    all_data_amended$GENDER == "1" ~ "Male",
+    all_data_amended$GENDER == "2" ~ "Female",
+    all_data_amended$GENDER == "3" ~ "Non-binary",
+    all_data_amended$GENDER == "4" ~ "Other"
+  ))
+
+
+#women overrepresented
+plot(all_data_amended$GENDERRAW, xlab = "Gender", ylab = "Participants",
+     main = "Gender Breakdown")
+
+#lots of ethnicities and not easy to combine so remove this demographic question
+plot(all_data_amended$ETHNICITY, xlab = "Ethnicities", ylab = "Participants",
+     main = "Ethnicity Breakdown")
+
+all_data_amended <- all_data_amended %>% select(!ETHNICITY)
+
+#add education column
+all_data_amended$EDUCATIONRAW <- as.factor(
+  case_when(
+    all_data_amended$EDUCATION == "1" ~ "No schooling",
+    all_data_amended$EDUCATION == "2" ~ "Primary school",
+    all_data_amended$EDUCATION == "3" ~ "Secondary school",
+    all_data_amended$EDUCATION == "4" ~ "College",
+    all_data_amended$EDUCATION == "5" ~ "Undergraduate",
+    all_data_amended$EDUCATION == "6" ~ "Masters",
+    all_data_amended$EDUCATION == "7" ~ "PhD"
+  ))
+
+#mostly undergrads, only a few PhD's
+plot(all_data_amended$EDUCATION, xlab = "Education", ylab = "Participants",
+     main = "Education Breakdown")
+
+#add political leaning column
+all_data_amended$LEANINGRAW <- as.factor(
+  case_when(
+    all_data_amended$LEANING == "1" ~ "Non political",
+    all_data_amended$LEANING == "2" ~ "Left wing",
+    all_data_amended$LEANING == "3" ~ "Centre",
+    all_data_amended$LEANING == "4" ~ "Right wing"
+  ))
+
+#not many right wing, imagine this would affect overall views!
+plot(all_data_amended$LEANINGRAW, xlab = "Political Leaning", ylab = "Participants",
+     main = "Political Leaning Breakdown")
+
+#can't be 100% sure of order because not in data dictionary
+# but have political leaning so this will suffice
+plot(all_data_amended$PARTY, xlab = "Political Party", ylab = "Participants",
+     main = "Political Party Breakdown")
+
+all_data_amended <- all_data_amended %>% select(!PARTY)
+
+
